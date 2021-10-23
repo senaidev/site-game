5710e32a0ef3db463f663bacb461de55c555e838
dev-willian <91983587+dev-willian@users.noreply.github.com>
2021-10-23 16:21:06 -0300

    segundo-commit

---
+++ scripts.js
@@ -0,0 +1,14 @@
+
+
+function menu() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }    
+   
+  window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+    let myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+    }
+  }
